ac2867da9bb3ba7c77a801e97337d1743ed4959e
foxy82 <foxy82.github@gmail.com>
2024-03-24 11:17:58 +0000

    Implemented

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
-
+FROM python:3.12-alpine
+WORKDIR /api
+COPY ./requirements.txt /api/requirements.txt
+RUN pip install --no-cache-dir -r /api/requirements.txt
+COPY ./ipchecker/ /api/
+CMD [ "uvicorn", "main:app", "--host", "0.0.0.0", "--port", "8000"]
--- ipchecker/main.py
+++ ipchecker/main.py
@@ -1,1 +1,29 @@
-# TODO Implement Fast API here
+from typing import Union
+from fastapi import FastAPI
+import requests
+
+app = FastAPI()
+
+
+def text_response(response):
+  return response.text.strip()
+
+ip_services = {
+  'https://checkip.amazonaws.com': text_response
+}
+
+
+@app.get("/")
+def read_root():
+  return { "Hello": "World" }
+
+@app.get("/ip")
+def get_external_ip():
+    service = 'https://checkip.amazonaws.com'
+    mapper_function = ip_services[service]
+    ip = mapper_function(requests.get(service))
+    return {"ip": ip}
+
+
+if __name__ == '__main__':
+  print(get_external_ip())
